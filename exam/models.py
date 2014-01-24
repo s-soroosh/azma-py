4d2e5b039d2df8719e8032879c451a198eb53082
Soroosh Sarabadani <Soroosh.Sarabadani@gmail.com>
2014-01-24 18:24:29 +0330

    initial_date.json added for exam app. now with syncdb command your database data will be initialized too.

--- exam/models.py
+++ exam/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Exam(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
     start_date = models.DateTimeField()
 
     def __str__(self):
