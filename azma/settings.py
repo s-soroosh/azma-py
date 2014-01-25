95b5689442a74d15679726610bf6a919b9462602
soroosh <soroosh.sarabadani@gmail.com>
2014-01-25 13:27:09 +0330

    Base Model created

--- azma/settings.py
+++ azma/settings.py
@@ -25,8 +25,9 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-
 ALLOWED_HOSTS = []
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
 
 
 # Application definition
@@ -84,3 +85,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+
