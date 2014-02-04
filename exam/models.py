1754ca2c704a89e95ea1d0dd96218ff628aefc2c
soroosh <soroosh.sarabadani@gmail.com>
2014-02-04 18:49:59 +0330

    Exam category added to model

--- exam/models.py
+++ exam/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class ExamCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    def number_of_exams(self):
+        return self.exam_set.count()
+
+
 class Exam(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateTimeField()
+    category = models.ForeignKey(ExamCategory)
 
 
     def __str__(self):
