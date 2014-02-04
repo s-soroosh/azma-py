1ddcf2f926fb73a754ac45002772ca5c940ec092
soroosh <soroosh.sarabadani@gmail.com>
2014-02-04 16:09:46 +0330

    Added a fake test to show email params

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.core.mail import send_mail
 
-# Create your tests here.
+
+def fake_test():
+    send_mail('SUBJECT ', 'Hello man', 'azmaweb@zareie.net', ['soroosh.sarabadani@gmail.com', 'mahdi.elf@gmail.com'],
+              fail_silently=False)
