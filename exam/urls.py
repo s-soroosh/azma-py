21f79cf1b14bc44ca0ad417b7fa3543c8f6fed9a
soroosh <soroosh.sarabadani@gmail.com>
2014-02-05 15:48:29 +0330

    a prototype for exam intro page created with countdown (it needs more effort to become functional and usable)

--- exam/urls.py
+++ exam/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from exam import views
 
 urlpatterns = patterns('',
-                       url(r'^(?i)$', views.index, name='home')
+                       url(r'^(?i)$', views.index, name='home'),
+                       url(r'^(?i)(?P<exam_id>\d)', views.intro, name='intro'),
 )
--- exam/views.py
+++ exam/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
+
 from exam.models import Exam
 
 
@@ -10,5 +11,16 @@ def index(request):
     template = loader.get_template('sample.html')
     context = RequestContext(request, {'all_exams': all_exams})
     return HttpResponse(template.render(context))
+
+
+def intro(request, exam_id):
+    if request.method == "GET":
+        exam = Exam.objects.get(id=exam_id)
+        template = loader.get_template('exam_intro.html')
+        context = RequestContext(request, {'exam': exam})
+        return HttpResponse(template.render(context))
+
+
+
 
 
