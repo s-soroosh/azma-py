876fefd6843889301392f38baf8b73b9aa947056
Mahdi <Mahdi@Zareie.Net>
2014-02-14 13:14:58 +0330

    wage_meter Models has been created.  details :  Models are created according to this wiki page : https://github.com/psycho-ir/azma-py/wiki/Model-Diagram-for-Wage_meter

--- wage_meter/models.py
+++ wage_meter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here  ...
+
+
+class Island(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    pass
+
+class Technology(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    parent = models.ManyToManyField('self')
+    pass
