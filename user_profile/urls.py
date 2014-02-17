22df4e8dc19e6bcddf973bb15755fdc70680aed4
soroosh <soroosh.sarabadani@gmail.com>
2014-02-17 15:58:23 +0330

    change password completed (But is does not have client validation)

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,5 +4,7 @@ from user_profile import views
 __author__ = 'soroosh'
 
 urlpatterns = patterns('',
-                       url(r'^(?i)$', views.show_profile, name='show_profile'))
+                       url(r'^(?i)$', views.show_profile, name='show_profile'),
+                       url(r'^(?i)changepass$', views.change_password, name='change_password'),
+                       )
 
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,11 +1,39 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
 from django.template import loader
 from django.template.context import RequestContext
+from django.utils.translation import ugettext_lazy as _
+
+profile_template = loader.get_template('profile.html')
 
 
 @login_required(login_url='/user/login')
 def show_profile(request):
-    template = loader.get_template('profile.html')
     context = RequestContext(request)
-    return HttpResponse(template.render(context))
+    return HttpResponse(profile_template.render(context))
+
+
+@login_required(login_url='/user/login')
+def change_password(request):
+    if request.method == "POST":
+        oldPass = request.POST["oldPass"]
+        newPass = request.POST["newPass"]
+
+        if request.user.check_password(oldPass):
+            request.user.set_password(newPass)
+            request.user.save()
+            context = RequestContext(request, {'message': _('password has been changed')})
+
+            return HttpResponse(profile_template.render(context))
+        else:
+            return HttpResponse("!!!!!")
+
+    else:
+        return HttpResponse(profile_template.render(RequestContext(request)))
+
+
+
+
+
+
