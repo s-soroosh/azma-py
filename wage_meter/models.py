34995079948e8720c24cb6b1ec24dc75df47470b
Mahdi <Mahdi@Zareie.Net>
2014-02-24 18:19:27 +0330

    wage_meter models has been changed. details: __str__ Methods has been implemented Technology.parent is not symmetrical anymore

--- wage_meter/models.py
+++ wage_meter/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Island(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
-    def __unicode__(self):
+
+    def __str__(self):
         return self.name
 
 
@@ -12,5 +13,8 @@ class Technology(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     ratio = models.FloatField()
-    parent = models.ManyToManyField("self")
+    parent = models.ManyToManyField("self", symmetrical=False)
     island = models.ForeignKey(Island)
+
+    def __str__(self):
+        return self.name
