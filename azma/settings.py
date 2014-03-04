97953ef370f11cc04577d871f3a3f57bd09905db
soroosh <soroosh.sarabadani@gmail.com>
2014-03-04 15:58:12 +0330

    STATIC_ROOT added to settings

--- azma/settings.py
+++ azma/settings.py
@@ -14,7 +14,9 @@ from django.conf.urls import include
 from django.core.urlresolvers import reverse
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # COMPRESS_CSS_FILTERS = [
 #     #creates absolute urls from relative ones
