8b9d1aa0aaedfe554ff08c2449507932e044eb37
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-03-10 14:28:11 +0330

    exam category improved

--- exam/urls.py
+++ exam/urls.py
@@ -4,7 +4,7 @@ from exam import views
 urlpatterns = patterns('',
                        url(r'^(?i)$', views.index, name='home'),
                        url(r'^(?i)what$', views.what, name='what'),
-                        url(r'^jjjjjjj(?i)(?P<exam_id>\d*)', views.intro, name='intro'),
-                         url(r'^(?i)(?P<category_id>\d*)', views.sub_categories, name='sub_categories')
+                        url(r'^exam/(?i)(?P<exam_id>\d*)', views.intro, name='intro'),
+                         url(r'^(?i)cat/(?P<category_id>\d*)', views.sub_categories, name='sub_categories')
 )
 
