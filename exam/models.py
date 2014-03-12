b936cd682122abd82e93590224a03d00f7047be0
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-03-12 11:06:02 +0330

    scoring added to models

--- exam/models.py
+++ exam/models.py
@@ -36,6 +36,7 @@ class RequiredKnowledge(models.Model):
 class Question(models.Model):
     text = models.CharField(max_length=500)
     exam = models.ForeignKey(Exam)
+    score = models.IntegerField(default=1)
 
     def __str__(self):
         return self.text
