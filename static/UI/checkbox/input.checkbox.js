701a7a8d8440a8d46a23c23a324af66919d4b94c
Mahdi <Mahdi@Zareie.Net>
2014-03-15 21:50:07 +0330

    duplicate jquery selectors has been removed for sake of performance

--- static/UI/checkbox/input.checkbox.js
+++ static/UI/checkbox/input.checkbox.js
@@ -1,17 +1,17 @@
         $(window).load(function () {
             $('input[type="checkbox"]').each(function (index, item) {
-                $(item).hide();
-                $(item).after('<div class="checkbox-mine"><span class="checkbox-mine-inside glyphicon glyphicon-ok"></span></div>');
-                $(item).change(function () {
+                var $item = $(item);
+                $item.hide();
+                $item.after('<div class="checkbox-mine"><span class="checkbox-mine-inside glyphicon glyphicon-ok"></span></div>');
+                $item.change(function () {
                     var chk = $(this);
-                    console.log(item);
                     if (chk.is(':checked')) {
-                        $(this).siblings().children().animate({
+                        chk.siblings().children().animate({
                             opacity: 1.0
                         }, 800);
                     }
                     else {
-                        $(this).siblings().children().animate({
+                        chk.siblings().children().animate({
                             opacity: 0.0
                         }, 200);
                     }
