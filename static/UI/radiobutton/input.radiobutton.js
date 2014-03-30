5bf20bc646bb9bb089f97e5eecea0713c88ae8ea
Mahdi <Mahdi@Zareie.Net>
2014-03-31 00:10:29 +0430

    UI for Exam's Start page has been improved. Details: UI Support Single-Answer and Multi-Answer Questions and use proper controls to display options .

---
+++ static/UI/radiobutton/input.radiobutton.js
@@ -0,0 +1,29 @@
+$(window).load(function () {
+    $('input[type="radio"]').each(function (index, item) {
+        var $item = $(item);
+        $item.hide();
+        $item.after('<div class="radio-mine"><span class="radio-mine-inside glyphicon glyphicon-pushpin"></span></div>');
+        $item.change(function () {
+            console.log(this);
+            var rdo = $(this);
+            var
+                rdo_name = rdo.attr('name'),
+                rdo_id = rdo.attr('id');
+
+            $('input[name="' + rdo_name + '"]').each(function (index, item) {
+                var rdo = $(item);
+                if (rdo.is(':checked')) {
+                    rdo.siblings().children().animate({
+                        opacity: 1.0
+                    }, 800);
+                }
+                else {
+                    rdo.siblings().children().animate({
+                        opacity: 0.0
+                    }, 200);
+                }
+
+            });
+        });
+    });
+});
