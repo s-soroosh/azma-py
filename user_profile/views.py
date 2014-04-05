2db0557631e5f0165dcfe64624f81d262661bd02
soroosh <soroosh.sarabadani@gmail.com>
2014-04-05 19:22:02 +0430

    profile exam results now is working with new model

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,7 +5,7 @@ from django.http.response import HttpResponse
 from django.template import loader
 from django.template.context import RequestContext
 from django.utils.translation import ugettext_lazy as _
-from answer.models import ExamAnswerHistory
+from answer.models import ExamAnswerHistory, ExamAnswer
 from exam.models import ExamCategory, Exam
 
 profile_template = loader.get_template('profile.html')
@@ -18,17 +18,17 @@ def show_profile(request):
     exam_categories = list(ExamCategory.objects.filter(exam__examanswer__user=1))
 
     for category in exam_categories:
-        category.answers = list(ExamAnswerHistory.objects.filter(user__id=request.user.id).filter(exam__category=category.id))
+        category.answers = list(ExamAnswer.objects.filter(user__id=request.user.id, exam__category=category.id))
         category.sum_score = 0
         for answer in category.answers:
-            category.sum_score += answer.score()
+            category.sum_score += answer.score
 
-    sum_score = 0
-    for a in exam_answers:
-        sum_score += a.score()
-    context = RequestContext(request,
-                             {'sum_score': sum_score, 'exam_categories': exam_categories, 'exam_answers': exam_answers})
-    return HttpResponse(profile_template.render(context))
+        sum_score = 0
+        for a in exam_answers:
+            sum_score += a.score()
+        context = RequestContext(request,
+        {'sum_score': sum_score, 'exam_categories': exam_categories, 'exam_answers': exam_answers})
+        return HttpResponse(profile_template.render(context))
 
 
 @login_required(login_url='/user/login')
