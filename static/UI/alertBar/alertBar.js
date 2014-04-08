fc1cc4a8712d57c8ea8a9cf4efc98bbbfb587c17
Mahdi <Mahdi@Zareie.Net>
2014-04-08 22:38:16 +0430

    alertBar Jquery Plugin has been created .

---
+++ static/UI/alertBar/alertBar.js
@@ -0,0 +1,48 @@
+/*
+ *Easy Usage :
+ * 1-include js and css in page After Jquery File
+ * 2-call window.alertBar(Error Message Text);
+ * attention : This function is not designed for displaying html messages and invoking alertBar with Html Text Content
+ * can lead you to unexpected behavior :-S
+ * */
+
+
+$("body").prepend("<div class='alertBar'></div>");
+window.alertbarBox = $(".alertBar");
+alertbarBox.mouseover(function () {
+    alertbarBox.stop().css("opacity", "1")
+});
+alertbarBox.mouseout(function () {
+    setTimeout(function () {
+        alertbarBox.animate(
+            {
+                "opacity": "0.0"
+            }
+            , 1000, function () {
+                alertbarBox.hide()
+            }
+        )
+    }, 2000)
+});
+console.log(alertbarBox);
+var alertBar = function (text) {
+    alertbarBox.html(text);
+    alertbarBox.show()
+        .animate(
+        {
+            "opacity": "1.0"
+        }
+        , 2000, function () {
+            setTimeout(function () {
+                alertbarBox.animate(
+                    {
+                        "opacity": "0.0"
+                    }
+                    , 1000, function () {
+                        alertbarBox.hide()
+                    }
+                )
+            }, 2000)
+        })
+}
+
