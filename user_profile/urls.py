d10983965814a1e809dbdc4d438cb92268a306c4
soroosh <soroosh.sarabadani@gmail.com>
2014-06-02 12:19:38 +0430

    Profile now is a view class

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 from user_profile import views
 
 __author__ = 'soroosh'
 
 urlpatterns = patterns('',
-                       url(r'^(?i)$', views.show_profile, name='show_profile'),
+                       url(r'^(?i)$', login_required(views.ProfileView.as_view()), name='show_profile'),
                        url(r'^(?i)/changepass$', views.change_password, name='change_password'),
                        )
 
