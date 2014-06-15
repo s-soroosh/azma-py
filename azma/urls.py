d1076e901ad8238046faba8588742ef94975e1e7
soroosh <soroosh.sarabadani@gmail.com>
2014-06-15 10:27:45 +0430

    /blog now is elephantblog

--- azma/urls.py
+++ azma/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
-
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from azma import views
 
 
@@ -13,6 +11,8 @@ urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'azma.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^blog/', include('elephantblog.urls')),
                        url(r'^weblog/', include('zinnia.urls')),
                        url(r'^comments/', include('django.contrib.comments.urls')),
                        url(r'^(?i)admin/', include(admin.site.urls)),
