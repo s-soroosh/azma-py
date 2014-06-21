d19750f6cacf74a62d6ad4a684db5a4551b74fa8
soroosh <soroosh.sarabadani@gmail.com>
2014-06-21 16:15:41 +0430

    footer created and facebook like and share added to it

--- answer/admin.py
+++ answer/admin.py
@@ -1,9 +1,17 @@
-from answer.models import ExamAnswer, ExamAnswerHistory, Answer
+# from answer.models import ExamAnswer, ExamAnswerHistory, Answer
 
-__author__ = 'soroosh'
+# __author__ = 'soroosh'
+#
+# from django.contrib import admin
+#
+# admin.site.register(ExamAnswer)
+# admin.site.register(Answer)
+# admin.site.register(ExamAnswerHistory)
 
-from django.contrib import admin
 
-admin.site.register(ExamAnswer)
-admin.site.register(Answer)
-admin.site.register(ExamAnswerHistory)
+from random import sample
+
+
+
+for a in range(100):
+    print str(a) +"time result: "+ str(sample(['a','b','c','d'],2))
