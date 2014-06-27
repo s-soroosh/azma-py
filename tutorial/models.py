749e08fc72347e0728e6b4caec1c3c477b112378
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-06-27 13:18:43 +0430

    keyword added to tutorial

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class TutorialCategory(models.Model):
-    name = models.CharField(max_length=120,primary_key=True)
-    local_name = models.CharField(max_length=120,)
+    name = models.CharField(max_length=120, primary_key=True)
+    local_name = models.CharField(max_length=120, )
     parent = models.ForeignKey("self", null=True, blank=True, related_name='sub_categories')
 
 
 class Tutorial(models.Model):
     category = models.ForeignKey(TutorialCategory)
-    name = models.CharField(max_length=120,primary_key=True)
+    keyword = models.CharField(max_length=200)  # comma separated
+    name = models.CharField(max_length=120, primary_key=True)
     local_name = models.CharField(max_length=150)
     author = models.ForeignKey(User)
     content = models.TextField()
