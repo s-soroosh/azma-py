1155a7b7a43e9a926c81609f29c988eef210c148
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2014-06-27 12:17:00 +0430

    First tutorial model created

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class TutorialCategory(models.Model):
+    name = models.CharField(max_length=120,primary_key=True)
+    local_name = models.CharField(max_length=120,)
+    parent = models.ForeignKey("self", null=True, blank=True, related_name='sub_categories')
+
+
+class Tutorial(models.Model):
+    category = models.ForeignKey(TutorialCategory)
+    name = models.CharField(max_length=120,primary_key=True)
+    local_name = models.CharField(max_length=150)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+    registered_date = models.DateTimeField()
+
--- tutorial/views.py
+++ tutorial/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 from django.template import loader
 from django.template.context import RequestContext
 from django.views.generic import View
+from tutorial.models import TutorialCategory
 
 
 class TutorialView(View):
     def get(self, request):
+        categories = TutorialCategory.objects.filter(parent_id=None)
+
         template = loader.get_template('tutorial_page.html')
-        context = RequestContext(request)
+
+        context = RequestContext(request, {'t_categories': categories})
         return HttpResponse(template.render(context))
