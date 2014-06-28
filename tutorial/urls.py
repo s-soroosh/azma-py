d058d408a281c54b839f51c6141234dc52765d65
soroosh <soroosh.sarabadani@gmail.com>
2014-06-28 10:53:33 +0430

    tutorial url and view created

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,4 +1,4 @@
-from tutorial.views import TutorialView, TutorialWithCategoryView
+from tutorial.views import *
 
 __author__ = 'SOROOSH'
 
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^$', TutorialView.as_view(), name="home"),
-                       url(r'^cat/(?P<category_name>.*)$', TutorialWithCategoryView.as_view(), name="category")
+                       url(r'^cat/(?P<category_name>.*)$', TutorialWithCategoryView.as_view(), name="category"),
+                       url(r'^(?P<tutorial_name>.*)$', TutorialDetailView.as_view(), name="tutorial")
 )
 
--- tutorial/views.py
+++ tutorial/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.template.context import RequestContext
 from django.views.generic import View
 
-from tutorial.models import TutorialCategory
+from tutorial.models import TutorialCategory, Tutorial
 
 
 class TutorialView(View):
@@ -18,9 +18,19 @@ class TutorialView(View):
 
 class TutorialWithCategoryView(View):
     def get(self, request, category_name):
-
         t_category = TutorialCategory.objects.get(name=category_name.upper())
         template = loader.get_template('category_detail.html')
 
         context = RequestContext(request, {'t_category': t_category})
+        return HttpResponse(template.render(context))
+
+
+class TutorialDetailView(View):
+    def get(self, request, tutorial_name):
+        tutorial = Tutorial.objects.get(name=tutorial_name.upper())
+        category = tutorial.category
+        while category.parent is not None:
+            category = category.parent
+        template = loader.get_template('tutorial_detail.html')
+        context = RequestContext(request, {'category': category, 'tutorial': tutorial})
         return HttpResponse(template.render(context))
