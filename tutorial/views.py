7e0e2ea007dca5cb77fbd7a4bd97a9f7434150bb
soroosh <soroosh.sarabadani@gmail.com>
2014-06-28 10:27:32 +0430

    Category view created

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
 from django.template.context import RequestContext
 from django.views.generic import View
+
 from tutorial.models import TutorialCategory
 
 
@@ -13,4 +13,14 @@ class TutorialView(View):
         template = loader.get_template('tutorial_page.html')
 
         context = RequestContext(request, {'t_categories': categories})
+        return HttpResponse(template.render(context))
+
+
+class TutorialWithCategoryView(View):
+    def get(self, request, category_name):
+
+        t_category = TutorialCategory.objects.get(name=category_name.upper())
+        template = loader.get_template('category_detail.html')
+
+        context = RequestContext(request, {'t_category': t_category})
         return HttpResponse(template.render(context))
