8d4990bb4a33959085d120ff733c964191e271be
soroosh <soroosh.sarabadani@gmail.com>
2014-06-28 10:27:19 +0430

    category part created

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,10 +1,11 @@
-from tutorial.views import TutorialView
+from tutorial.views import TutorialView, TutorialWithCategoryView
 
 __author__ = 'SOROOSH'
 
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-                       url(r'^$', TutorialView.as_view(), name="home")
+                       url(r'^$', TutorialView.as_view(), name="home"),
+                       url(r'^cat/(?P<category_name>.*)$', TutorialWithCategoryView.as_view(), name="category")
 )
 
