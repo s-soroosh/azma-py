799059fe99ce240b4da76b835d7f5aa3522da15f
soroosh <soroosh.sarabadani@gmail.com>
2014-06-28 17:41:29 +0430

    comment added to tutorial with gravatar support

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,10 +4,16 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from exam.models import Exam
+import hashlib
 
-#################################
+# ################################
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
+
+    def email_md5(self):
+
+        hashlib.md5
+        return str(hashlib.md5(self.user.email.lower()).hexdigest())
 
 
 def create_user_profile(sender, instance, created, **kwargs):
