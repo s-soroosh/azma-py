47895e7a39f65362e8a674b9597e4788db9775b5
soroosh <soroosh.sarabadani@gmail.com>
2014-06-28 10:27:04 +0430

    model changed to support description

--- tutorial/models.py
+++ tutorial/models.py
@@ -6,10 +6,11 @@ class TutorialCategory(models.Model):
     name = models.CharField(max_length=120, primary_key=True)
     local_name = models.CharField(max_length=120, )
     parent = models.ForeignKey("self", null=True, blank=True, related_name='sub_categories')
+    description = models.TextField(default="Without description")
 
 
 class Tutorial(models.Model):
-    category = models.ForeignKey(TutorialCategory)
+    category = models.ForeignKey(TutorialCategory,related_name='tutorials')
     keyword = models.CharField(max_length=200)  # comma separated
     name = models.CharField(max_length=120, primary_key=True)
     local_name = models.CharField(max_length=150)
