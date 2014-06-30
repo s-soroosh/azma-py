f70c8cbebdfcb9392aecf9acf351f6a044bce79b
Sajad <sajjad.sarabadani@gmail.com>
2014-06-30 15:40:51 +0430

    fixed parent menus added.....

--- tutorial/views.py
+++ tutorial/views.py
@@ -18,22 +18,24 @@ class TutorialView(View):
 
 class TutorialWithCategoryView(View):
     def get(self, request, category_name):
+        categories = TutorialCategory.objects.filter(parent_id=None)
         t_category = TutorialCategory.objects.get(name=category_name.upper())
         category = t_category
         while category.parent is not None:
             category = category.parent
         template = loader.get_template('category_detail.html')
 
-        context = RequestContext(request, {'category': category, 't_category': t_category})
+        context = RequestContext(request, {'category': category, 't_category': t_category, 't_categories': categories})
         return HttpResponse(template.render(context))
 
 
 class TutorialDetailView(View):
     def get(self, request, tutorial_name):
+        categories = TutorialCategory.objects.filter(parent_id=None)
         tutorial = Tutorial.objects.get(name=tutorial_name.upper())
         category = tutorial.category
         while category.parent is not None:
             category = category.parent
         template = loader.get_template('tutorial_detail.html')
-        context = RequestContext(request, {'category': category, 'tutorial': tutorial})
+        context = RequestContext(request, {'category': category, 'tutorial': tutorial, 't_categories': categories})
         return HttpResponse(template.render(context))
