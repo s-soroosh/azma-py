76de32ab3f313bc075b1bfad8aca7caad5d77f3c
soroosh <soroosh.sarabadani@gmail.com>
2014-07-03 11:33:45 +0430

    latest tutorials added

--- tutorial/views.py
+++ tutorial/views.py
@@ -9,10 +9,11 @@ from tutorial.models import TutorialCategory, Tutorial
 class TutorialView(View):
     def get(self, request):
         categories = TutorialCategory.objects.filter(parent_id=None)
+        latest_tutorials = Tutorial.objects.all()[:5]
 
         template = loader.get_template('tutorial_page.html')
 
-        context = RequestContext(request, {'t_categories': categories})
+        context = RequestContext(request, {'latest_tutorials': latest_tutorials, 't_categories': categories})
         return HttpResponse(template.render(context))
 
 
@@ -21,11 +22,14 @@ class TutorialWithCategoryView(View):
         categories = TutorialCategory.objects.filter(parent_id=None)
         t_category = TutorialCategory.objects.get(name=category_name.upper())
         category = t_category
+        latest_tutorials = Tutorial.objects.all()[:5]
         while category.parent is not None:
             category = category.parent
         template = loader.get_template('category_detail.html')
 
-        context = RequestContext(request, {'category': category, 't_category': t_category, 't_categories': categories})
+        context = RequestContext(request,
+                                 {'latest_tutorials': latest_tutorials, 'category': category, 't_category': t_category,
+                                  't_categories': categories})
         return HttpResponse(template.render(context))
 
 
@@ -33,9 +37,12 @@ class TutorialDetailView(View):
     def get(self, request, tutorial_name):
         categories = TutorialCategory.objects.filter(parent_id=None)
         tutorial = Tutorial.objects.get(name=tutorial_name.upper())
+        latest_tutorials = Tutorial.objects.all()[:5]
         category = tutorial.category
         while category.parent is not None:
             category = category.parent
         template = loader.get_template('tutorial_detail.html')
-        context = RequestContext(request, {'category': category, 'tutorial': tutorial, 't_categories': categories})
+        context = RequestContext(request,
+                                 {'latest_tutorials': latest_tutorials, 'category': category, 'tutorial': tutorial,
+                                  't_categories': categories})
         return HttpResponse(template.render(context))
