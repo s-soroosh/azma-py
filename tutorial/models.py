56323e94f2614e5ba088d056a9c9675e872a4090
soroosh <soroosh.sarabadani@gmail.com>
2014-07-03 11:31:19 +0430

    order added to category and ordering added to models

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,22 +1,27 @@
 from django.contrib.auth.models import User
 from django.db import models
-from feincms.contrib.richtext import RichTextField
 from jdatetime import datetime as jalali_datetime
 from tinymce.models import HTMLField
 
 
 class TutorialCategory(models.Model):
+    class Meta:
+        ordering = ['order']
     name = models.CharField(max_length=120, primary_key=True)
     local_name = models.CharField(max_length=120, )
     parent = models.ForeignKey("self", null=True, blank=True, related_name='sub_categories')
     description = HTMLField(default="Without description")
     img_address = models.CharField(default='image/techs/python.png', max_length=200)
+    order = models.IntegerField(default=1)
 
     def __unicode__(self):
         return self.local_name
 
 
 class Tutorial(models.Model):
+    class Meta:
+        ordering = ['registered_date']
+
     category = models.ForeignKey(TutorialCategory, related_name='tutorials')
     keyword = models.CharField(max_length=200)  # comma separated
     abstract = HTMLField()
